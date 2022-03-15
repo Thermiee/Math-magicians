b99071294eb81e533820f2e340221241088b9728
Thermiee <navyblack16@yahoo.com>
2022-03-15 20:28:21 +0100

    Add class calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Calculator extends Component {
+  render() {
+    return (
+      <div>Calculator</div>
+    )
+  }
+}
+
+export default Calculator
