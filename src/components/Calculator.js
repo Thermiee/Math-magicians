f78f780932b56ce70f5655f994326795a8338e23
Thermiee <navyblack16@yahoo.com>
2022-03-16 23:24:59 +0100

    add buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
 export default function Calculator() {
+  const numbers = () => {
+    const digits = [];
+
+    for (let i = 1; i < 10; i += 1) {
+      digits.push(
+        <button type="button" key={i}>{i}</button>,
+      );
+    }
+  };
+
   return (
-    <div className ="App">
+    <div className="App">
       <div className="Calculator">
         <div className="Answers">
           <span>0</span>
         </div>
-        <div>Calculator</div>
+
+        <div className="operators">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">/</button>
+        </div>
+
+        <div className="digits">
+          { numbers() }
+          <button type="button">0</button>
+          <button type="button">0</button>
+        </div>
       </div>
     </div>
   );
