d0b7597301edd83789588c8fa20ffa869bdc4d0e
Thermiee <navyblack16@yahoo.com>
2022-03-17 13:58:19 +0100

    Add calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,36 +1,47 @@
 import React from 'react';
+import '../index.css';
 
-export default function Calculator() {
-  const numbers = () => {
-    const digits = [];
-
-    for (let i = 1; i < 10; i += 1) {
-      digits.push(
-        <button type="button" key={i}>{i}</button>,
-      );
-    }
-  };
-
+function Calculator() {
   return (
-    <div className="App">
-      <div className="Calculator">
-        <div className="Answers">
-          <span>0</span>
+    <div className="calculator">
+      <div className="calculator__view__container">
+        <div className="calculator__view">
+          <h1 className="calculator__view__h1">0</h1>
         </div>
-
-        <div className="operators">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button">/</button>
+      </div>
+      <div className="calculator__rows">
+        <div className="calculator__row">
+          <button type="button" className="calculator__greyButton">AC</button>
+          <button type="button" className="calculator__greyButton">+/-</button>
+          <button type="button" className="calculator__greyButton">%</button>
+          <button type="button" className="calculator__orangeButton">÷</button>
         </div>
-
-        <div className="digits">
-          { numbers() }
-          <button type="button">0</button>
-          <button type="button">0</button>
+        <div className="calculator__row">
+          <button type="button" className="calculator__greyButton">7</button>
+          <button type="button" className="calculator__greyButton">8</button>
+          <button type="button" className="calculator__greyButton">9</button>
+          <button type="button" className="calculator__orangeButton">x</button>
+        </div>
+        <div className="calculator__row">
+          <button type="button" className="calculator__greyButton">4</button>
+          <button type="button" className="calculator__greyButton">5</button>
+          <button type="button" className="calculator__greyButton">6</button>
+          <button type="button" className="calculator__orangeButton ">—</button>
+        </div>
+        <div className="calculator__row">
+          <button type="button" className="calculator__greyButton">1</button>
+          <button type="button" className="calculator__greyButton">2</button>
+          <button type="button" className="calculator__greyButton">3</button>
+          <button type="button" className="calculator__orangeButton ">+</button>
+        </div>
+        <div className="calculator__row">
+          <button type="button" className="calculator__greyButtonZero">0</button>
+          <button type="button" className="calculator__greyButton">.</button>
+          <button type="button" className="calculator__orangeButton">=</button>
         </div>
       </div>
     </div>
   );
 }
+
+export default Calculator;
