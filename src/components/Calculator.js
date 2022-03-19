dbe53310017019eadf9c97fda6cac30ec458f6ef
Thermiee <navyblack16@yahoo.com>
2022-03-19 15:19:24 +0100

    refactor using hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,48 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
 const operators = ['÷', 'x', '-', '+', '='];
 const digits = ['AC', '+/-', '%', '7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.'];
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      next: '',
-      total: '',
-      operation: '',
-    };
-  }
+const Calculator = () => {
+  const [obj, calcObj] = useState({
+    next: '',
+    total: '',
+    operation: '',
+  });
 
-  handleClick = (btn) => {
-    this.setState((state) => calculate(
-      {
-        next: state.next,
-        total: state.total,
-        operation: state.operation,
-      },
-      btn,
-    ));
-  }
+  const handleClick = (btn) => {
+    const { total, next, operation } = calculate(obj, btn);
+    if (next === null && total === null) {
+      calcObj({ next, total: '', operation });
+    } else {
+      calcObj({ next, total, operation });
+    }
+  };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <ul className="calculator-body">
-        <li className="calculator-screen">
-          {next || total || '0'}
-        </li>
-        <li className="calculator-keyboard">
-          <div className="calculator-digits">
-            {digits.map((btn) => <button onClick={() => this.handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
-          </div>
-          <div className="calculator-operators">
-            {operators.map((btn) => <button onClick={() => this.handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
-          </div>
-        </li>
-      </ul>
-    );
-  }
-}
-
+  return (
+    <ul className="calculator-body">
+      <li className="calculator-screen">
+        {obj.next || obj.total || '0'}
+      </li>
+      <li className="calculator-keyboard">
+        <div className="calculator-digits">
+          {digits.map((btn) => <button onClick={() => handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
+        </div>
+        <div className="calculator-operators">
+          {operators.map((btn) => <button onClick={() => handleClick(btn)} type="button" key={btn}>{ btn }</button>) }
+        </div>
+      </li>
+    </ul>
+  );
+};
 export default Calculator;
